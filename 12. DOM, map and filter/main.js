b764b6f06b2b966d08f7da602f389929c8813df5
Amalie <Amal956e@stud.kea.dk>
2022-10-04 18:22:52 +0200

    Øvelse til forberedelse til "Doom, map and filter"

---
+++ 12. DOM, map and filter/main.js
@@ -0,0 +1,45 @@
+// Map - Laver en ny array
+/*
+const people = [
+    {
+        name: "Amalie",
+        age: 35,
+        occupation: "Studerende"
+    },
+    {
+        name: "Julie",
+        age: 40,
+        occupation: "Studerende"
+    },
+    {
+        name: "Cecilie",
+        age: 50,
+        occupation: "Studerende"
+    },
+];
+
+const names = people.map(person => {
+    return person.name;
+});
+
+console.log(names); // [ 'Amalie', 'Julie', 'Cecilie' ]
+
+// Filter - Filter Amalie og julie fra
+const over50 = people.filter(person => {
+    return person.age >=50;
+}).map(person => {
+    return person.name;
+});
+
+console.log(over50); // [ 'Cecilie' ]
+
+
+//Reduce method - reducer en array element.
+const totalAge = people.reduce((total, person) => {
+    return total + person.age;
+}, 0);
+console.log(totalAge);
+*/
+
+// Exercise 1 - level 1.
+
